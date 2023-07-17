71ab11cae35ce729b3333b6d0d535aca3a6ba6c9
ashlin07 <furtadoashlin07@gmail.com>
2023-07-17 17:04:23 +0530

    commit

--- model_on_video.py
+++ model_on_video.py
@@ -1,49 +1,66 @@
-import cv2
-import socket
-import pickle
-import struct
-import mediapipe as mp
-import tensorflow as tf
 from keras.models import load_model
+import cv2
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
 import numpy as np
-
-
+import tensorflow as tf
+#Video Feed
 def run_model(path):
-    
-    
-    
-  
-        model = load_model("model_instrusion.h5")
+    cap = cv2.VideoCapture(path)
+    with mp_pose.Pose(min_detection_confidence = 0.5,min_tracking_confidence = 0.5) as pose:
+        while cap.isOpened():
+            ret,frame = cap.read()
+            if ret:
+                cv2.namedWindow("Mediapipe_Feed", cv2.WINDOW_NORMAL)
+                cv2.resizeWindow("Mediapipe_Feed", 432,768)
+            #Recolor to RGB
+                image = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+                image.flags.writeable = False
+
+
+                #Mediapipe Pose estimation
+                results = pose.process(image)
+                
+                #Recolor to BGR
+                image.flags.writeable = True
+                image = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
+
+                pose1 = results.pose_landmarks.landmark
+                # print(pose1)
+
+                pose_row = list(np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility]for landmark in pose1]).flatten())
+                
+                model = load_model("model_instrusion.h5")
+            
+                # Check if the person is standing based on the ankle y-coordinate difference
+                prediction = model.predict(pose_row.reshape(1,33*(4)),verbose=None)
+                prediction_p = tf.nn.softmax(prediction)
+                yhat = np.argmax(prediction_p)
+                if yhat== 0:  # Adjust the threshold as needed
+                    text = "Normal"
+                elif yhat==1:
+                    text = "Looking back left"
+                elif yhat==2:
+                    text="Looking back right"
+                else:
+                    text="Suspicious"
+                
+                # Display the standing status on the frame
+                cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                mp_drawing.draw_landmarks(image,results.pose_landmarks,mp_pose.POSE_CONNECTIONS)
+                # Display the frame
+                cv2.imshow("Atm intrusion detection", frame)
+                
+                
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
+                
+            else:
+                break
         
-        # Check if the person is standing based on the ankle y-coordinate difference
-        prediction = model.predict(pose_row.reshape(1,33*(4)),verbose=None)
-        prediction_p = tf.nn.softmax(prediction)
-        yhat = np.argmax(prediction_p)
-        if yhat== 0:  # Adjust the threshold as needed
-            text = "Normal"
-        elif yhat==1:
-            text = "Looking back left"
-        elif yhat==2:
-            text="Looking back right"
-        else:
-            text="Suspicious"
-        
-        # Display the standing status on the frame
-        cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-        
-        # Display the frame
-        cv2.imshow("Atm intrusion detection", frame)
+    cap.release()
+    cv2.destroyAllWindows()
 
-        # Check for the 'q' key to quit the loop
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    
-    # Close the client socket and the server socket
-    client_socket.close()
-    server_socket.close()
-
-
-
-
-receive_live_video()
+run_model()
 
