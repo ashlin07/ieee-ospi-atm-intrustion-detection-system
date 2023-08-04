72be73be69729401f187a00d96573609f0c42ec4
ashlin07 <furtadoashlin07@gmail.com>
2023-08-04 16:34:16 +0530

    added multiple people detection

---
+++ multiple_people.py
@@ -0,0 +1,123 @@
+import os
+import time
+import cv2
+import numpy as np
+from model.yolo_model import YOLO
+
+def process_image(img):
+    """Resize, reduce and expand image.
+
+    # Argument:
+        img: original image.
+
+    # Returns
+        image: ndarray(64, 64, 3), processed image.
+    """
+    image = cv2.resize(img, (416, 416),
+                       interpolation=cv2.INTER_CUBIC)
+    image = np.array(image, dtype='float32')
+    image /= 255.
+    image = np.expand_dims(image, axis=0)
+
+    return image
+
+
+def draw(image, boxes, scores, classes, all_classes):
+    """Draw the boxes on the image.
+
+    # Argument:
+        image: original image.
+        boxes: ndarray, boxes of objects.
+        classes: ndarray, classes of objects.
+        scores: ndarray, scores of objects.
+        all_classes: all classes name.
+    """
+    for box, score, cl in zip(boxes, scores, classes):
+        x, y, w, h = box
+
+        top = max(0, np.floor(x + 0.5).astype(int))
+        left = max(0, np.floor(y + 0.5).astype(int))
+        right = min(image.shape[1], np.floor(x + w + 0.5).astype(int))
+        bottom = min(image.shape[0], np.floor(y + h + 0.5).astype(int))
+        if cl==0:
+            cv2.rectangle(image, (top, left), (right, bottom), (255, 0, 0), 2)
+            cv2.putText(image, '{0} {1:.2f}'.format(all_classes[cl], score),
+                        (top, left - 6),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.6, (0, 0, 255), 1,
+                        cv2.LINE_AA)
+   
+    print()
+
+def detect_image(image, yolo, all_classes):
+    """Use yolo v3 to detect images.
+
+    # Argument:
+        image: original image.
+        yolo: YOLO, yolo model.
+        all_classes: all classes name.
+
+    # Returns:
+        image: processed image.
+    """
+    pimage = process_image(image)
+
+    
+    boxes, classes, scores = yolo.predict(pimage, image.shape)
+    if boxes is not None:
+        print(len(boxes))
+        if(len(boxes>1)):
+            print("mulitple people")
+        draw(image, boxes, scores, classes, all_classes)
+
+    return image
+
+
+def detect_video(video, yolo, all_classes):
+    """Use yolo v3 to detect video.
+
+    # Argument:
+        video: video file.
+        yolo: YOLO, yolo model.
+        all_classes: all classes name.
+    """
+    video_path = os.path.join("videos", "test", video)
+    camera = cv2.VideoCapture(video_path)
+    cv2.namedWindow("detection", cv2.WINDOW_AUTOSIZE)
+
+    # Prepare for saving the detected video
+    sz = (int(camera.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    fourcc = cv2.VideoWriter_fourcc(*'mpeg')
+
+    
+    vout = cv2.VideoWriter()
+    vout.open(os.path.join("videos", "res", video), fourcc, 20, sz, True)
+
+    while True:
+        res, frame = camera.read()
+
+        if not res:
+            break
+
+        image = detect_image(frame, yolo, all_classes)
+        cv2.imshow("detection", image)
+
+        # Save the video frame by frame
+        vout.write(image)
+
+        if cv2.waitKey(110) & 0xff == 27:
+                break
+
+    vout.release()
+    camera.release()
+
+
+
+yolo = YOLO(0.6, 0.5)
+all_classes=["person"]
+
+path = 'people.jpg'
+image = cv2.imread(path)
+image = detect_image(image, yolo, all_classes)
+cv2.imwrite('res/' + path, image)
